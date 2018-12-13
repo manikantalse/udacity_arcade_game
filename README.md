d5732b0b46e0c11d37968480114a3030a696d789
Manikanta L.S.E <manikanta.lse@gmail.com>
2018-12-14 03:53:37 +0530

    First commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# Classic Arcade Game
+This game is one of those good ol Arcade games where the player has to reach some destination to win while avoiding some obstacles.
+
+## How to run
+
+* ```git clone https://github.com/manikantalse/udacity_arcade_game.git```
+* ```cd udacity_arcade_game```
+* Now open index.html in your favourite browser
+
+## How to play
+
+Movement 
+
+* Use the Arrow keys (not the num key ones) to move the character around
+
+## Rules
+
+* Move around to avoid hitting the bugs
+* Aim towards reaching the water 
+
+## States
+
+* You win when you reach the water
+* You lose when you hit a bug
---
+++ js/app.js
@@ -0,0 +1,139 @@
+const WIDTH = 505;
+const HEIGHT = 606;
+
+// positions for the bugs across pavement
+const magicalPositions = [1, 2.4, 3.8];
+
+// modal div for reset
+const modal = document.querySelector(".modal");
+
+// variables
+let spawner, lost = false,allEnemies = [];
+
+
+// Enemy class
+
+class Enemy {
+
+    // initialization
+    constructor(x, y) {
+        this.sprite = 'images/enemy-bug.png';
+        this.x = x;
+        this.y = y;
+
+        // some random speed
+        this.speed = random(5, 15);
+    }
+
+    // update the enemy position
+    update(dt) {
+        this.x += this.speed;
+
+        // remove the bugs after they cross the screen as they are unwanted
+        if (this.x > WIDTH) {
+            allEnemies.splice(allEnemies.indexOf(this), 1);
+        }
+    };
+
+    // drawing the bug onto the canvas
+    render() {
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    };
+};
+
+
+// Player class 
+
+class Player {
+
+    // initialization
+    constructor(x, y) {
+        this.sprite = 'images/char-boy.png';
+        this.x = x;
+        this.y = y;
+    }
+
+    // handling arrow inputs
+    handleInput(direction) {
+        if (!direction || lost) return;
+
+        switch (direction) {
+            case 'left': player.x > 0 ? player.x -= 101 : 0; break;
+            case 'up': player.y > 0 ? (player.y < 80 ? (player.y -= 80, setTimeout(() => { reset("won") }, 100)) : player.y -= 80) : 0; break;
+            case 'right': player.x < 404 ? player.x += 101 : 0; break;
+            case 'down': player.y < 303 ? player.y += 80 : 0; break;
+            default: break;
+        }
+    }
+
+    // checking for collisions in update func...nothing more
+    update() {
+        allEnemies.forEach(function (enemy) {
+            if ((player.x + 20) < enemy.x + 101 && (player.x + 20) + 60 > enemy.x && (player.y + 120) < (enemy.y + 60) + 100 && (player.y + 120) + 10 > (enemy.y + 60)) {
+                lost = true;
+                setTimeout(() => { reset("lost") }, 5);
+            }
+        })
+    }
+
+    // drawing onto the canvas
+    render() {
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    }
+}
+
+// we will be using single player until the end
+const player = new Player(2 * 101, 5 * 75);
+
+// arrow key handling
+document.addEventListener('keyup', function (e) {
+
+    const allowedKeys = {
+        37: 'left',
+        38: 'up',
+        39: 'right',
+        40: 'down'
+    };
+
+    player.handleInput(allowedKeys[e.keyCode]);
+});
+
+// start the game 
+start();
+
+// start function definition for assigning few game restart variables
+function start() {
+    lost = false;
+    [player.x,player.y] = [2 * 101, 5 * 75];
+    modal.style.display = 'none';
+    document.querySelectorAll(".status").forEach(function (elem) {
+        elem.style.display = "none";
+    })
+    
+    // added 3 enemies at the start so player won't have time to quickly move across the pavement during the first 500 ms
+    for (let i = 0; i < 3; i++) {
+        allEnemies.push(new Enemy(-101, magicalPositions[random(3)] * 60));
+    }
+
+    // restart the counter for bugs spawning
+    spawner = setInterval(function () {
+        allEnemies.push(new Enemy(-101, magicalPositions[random(3)] * 60));
+    }, 500);
+}
+
+// function random returns between integers including r1 and excluding r2
+function random(r1, r2) {
+    let rand = Math.random();
+    return r2 ? Math.floor(rand * r2) + r1 : Math.floor(rand * r1);
+}
+
+
+// resetting the game after winning or losing
+function reset(status) {
+    clearInterval(spawner);
+    allEnemies = [];
+    modal.style.display = "block";
+    document.querySelectorAll("." + status).forEach(function (elem) {
+        elem.style.display = "block";
+    })
+}
